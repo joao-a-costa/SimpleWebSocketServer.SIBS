dfe30451745ff6482b84b4f1b93845de075d43d5
João Costa <joaomacostadev@gmail.com>
2024-07-05 16:52:18 +0100

    Bug fixes regarding starting and stopping.

--- SimpleWebSocketServer.SIBS/WebSocketServerSibs.cs
+++ SimpleWebSocketServer.SIBS/WebSocketServerSibs.cs
@@ -7,9 +7,20 @@ namespace SimpleWebSocketServer.SIBS
 {
     public class WebSocketServerSibs
     {
+        #region "Fields"
+
+        /// <summary>
+        /// The WebSocket server
+        /// </summary>
         private WebSocketServer server;
+        private Task serverTask;
+
+        #endregion
+
+        #region "Events"
 
         public delegate void ClientConnectedEventHandler(object sender, EventArgs e);
+        public delegate void ClientDisconnectedEventHandler(object sender, EventArgs e);
         public delegate void TerminalStatusReqResponseReceivedEventHandler(object sender, TerminalStatusReqResponse reqResponse);
         public delegate void SetAuthCredentialsReqResponseEventHandler(object sender, SetAuthCredentialsReqResponse reqResponse);
         public delegate void ProcessPaymentReqResponseEventHandler(object sender, ProcessPaymentReqResponse reqResponse);
@@ -17,11 +28,20 @@ namespace SimpleWebSocketServer.SIBS
         public delegate void ErrorNotificationEventHandler(object sender, ErrorNotification reqResponse);
 
         public event ClientConnectedEventHandler ClientConnected;
+        public event ClientDisconnectedEventHandler ClientDisconnected;
         public event TerminalStatusReqResponseReceivedEventHandler TerminalStatusReqResponseReceived;
         public event SetAuthCredentialsReqResponseEventHandler SetAuthCredentialsReqReceived;
         public event ProcessPaymentReqResponseEventHandler ProcessPaymentReqReceived;
         public event EventNotificationEventHandler EventNotificationReceived;
         public event ErrorNotificationEventHandler ErrorNotificationReceived;
+
+        #endregion
+
+        #region "Properties"
+
+        public bool IsStarted => server.IsStarted;
+
+        #endregion
 
         #region "Public methods"
 
@@ -29,7 +49,7 @@ namespace SimpleWebSocketServer.SIBS
         /// Start the WebSocket server
         /// </summary>
         /// <param name="prefix">The prefix to listen to</param>
-        public async Task StartAsync(string prefix)
+        public void Start(string prefix)
         {
             // Create an instance of WebSocketServer
             server = new WebSocketServer(prefix);
@@ -39,10 +59,11 @@ namespace SimpleWebSocketServer.SIBS
                 // Define events for server lifecycle
                 server.ServerStarted += Server_ServerStarted;
                 server.ClientConnected += Server_ClientConnected;
+                server.ClientDisconnected += Server_ClientDisconnected;
                 server.MessageReceived += Server_MessageReceived;
 
                 // Start the WebSocket server
-                await server.Start();
+                serverTask = server.Start();
             }
             catch (Exception ex)
             {
@@ -50,20 +71,53 @@ namespace SimpleWebSocketServer.SIBS
             }
         }
 
+        /// <summary>
+        /// Send a message to the client
+        /// </summary>
+        /// <param name="message">The message to send</param>
+        /// <returns>The task</returns>
         public async Task SendMessageToClient(string message)
         {
             await server.SendMessageToClient(message);
         }
 
+        /// <summary>
+        /// Stop the WebSocket server
+        /// </summary>
+        /// <returns>The task</returns>
+        public void Stop()
+        {
+            server.Stop();
+            serverTask = null;
+        }   
+
         #endregion
 
         #region "Private methods"
 
+        /// <summary>
+        /// OnClientConnected event handler
+        /// </summary>
         private void OnClientConnected()
         {
             ClientConnected?.Invoke(this, new EventArgs());
         }
 
+        /// <summary>
+        /// OnClientDisconnected event handler
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The message</param>
+        private void Server_ClientDisconnected(object sender, string e)
+        {
+            ClientDisconnected?.Invoke(this, new EventArgs());
+        }
+
+        /// <summary>
+        /// OnMessageReceived event handler
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The message</param>
         private void Server_MessageReceived(object sender, string e)
         {
             try
@@ -108,36 +162,66 @@ namespace SimpleWebSocketServer.SIBS
             }
         }
 
+        /// <summary>
+        /// OnClientConnected event handler
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The message</param>
         private void Server_ClientConnected(object sender, string e)
         {
             OnClientConnected();
         }
 
+        /// <summary>
+        /// OnServerStarted event handler
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The message</param>
         private void Server_ServerStarted(object sender, string e)
         {
             // To be implemented
         }
 
+        /// <summary>
+        /// OnTerminalStatusReqResponseReceived event handler
+        /// </summary>
+        /// <param name="reqResponse">The response</param>
         private void OnTerminalStatusReqResponseReceived(TerminalStatusReqResponse reqResponse)
         {
             TerminalStatusReqResponseReceived?.Invoke(this, reqResponse);
         }
 
+        /// <summary>
+        /// OnSetAuthCredentialsReqReceived event handler
+        /// </summary>
+        /// <param name="reqResponse">The response</param>
         private void OnSetAuthCredentialsReqReceived(SetAuthCredentialsReqResponse reqResponse)
         {
             SetAuthCredentialsReqReceived?.Invoke(this, reqResponse);
         }
 
+        /// <summary>
+        /// OnProcessPaymentReqReceived event handler
+        /// </summary>
+        /// <param name="reqResponse">The response</param>
         private void OnProcessPaymentReqReceived(ProcessPaymentReqResponse reqResponse)
         {
             ProcessPaymentReqReceived?.Invoke(this, reqResponse);
         }
 
+        /// <summary>
+        /// OnEventNotificationReceived event handler
+        /// </summary>
+        /// <param name="reqResponse">The response</param>
         private void OnEventNotificationReceived(EventNotification reqResponse)
         {
             EventNotificationReceived?.Invoke(this, reqResponse);
         }
 
+        /// <summary>
+        /// OnErrorNotificationReceived event handler
+        /// </summary>
+        /// <param name="reqResponse">The response</param>
         private void OnErrorNotificationReceived(ErrorNotification reqResponse)
         {
             ErrorNotificationReceived?.Invoke(this, reqResponse);
