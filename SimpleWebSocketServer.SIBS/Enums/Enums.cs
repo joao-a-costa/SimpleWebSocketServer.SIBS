c459ecf64c3c20ac24ab85ebcbcb79c69d332d63
João Costa <joaomacostadev@gmail.com>
2024-07-23 10:33:03 +0100

    SIBS Hearbeat and transaction response implementation

--- SimpleWebSocketServer.SIBS/Enums/Enums.cs
+++ SimpleWebSocketServer.SIBS/Enums/Enums.cs
@@ -11,7 +11,10 @@
             PROCESS_PAYMENT_REQUEST,
             PROCESS_PAYMENT_RESPONSE,
             EVENT_NOTIFICATION,
-            ERROR_NOTIFICATION
+            ERROR_NOTIFICATION,
+            HEARTBEAT_NOTIFICATION,
+            TX_REQUEST,
+            TX_RESPONSE
         }
 
         public enum Version
