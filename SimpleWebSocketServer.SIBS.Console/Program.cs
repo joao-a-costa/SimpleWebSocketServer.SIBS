dcb79e3e7a6c1148305ddc8df7bf1ab52a330e0b
João Costa <joaomacostadev@gmail.com>
2024-09-06 09:25:19 +0100

    Code improvements

--- SimpleWebSocketServer.SIBS.Console/Program.cs
+++ SimpleWebSocketServer.SIBS.Console/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using SimpleWebSocketServer.SIBS.Lib;
 using SimpleWebSocketServer.SIBS.Lib.Models;
+using static SimpleWebSocketServer.SIBS.Lib.Enums.Enums;
 
 namespace SimpleWebSocketServer.SIBS.Console
 {
@@ -11,70 +13,39 @@ namespace SimpleWebSocketServer.SIBS.Console
         #region "Constants"
 
         private const string _WebSocketServerPrefix = "https://+:10005/";
-        private const string _MessageEnterJSONCommand = "Enter JSON command or 'q' to stop:";
+        private const string _MessageEnterJSONCommand = "Enter 'q' to stop:";
         private const string _MessageErrorErrorOccurred = "Error occurred";
-        private const string _MessageErrorProcessingJSON = "Error processing JSON";
+        private const string _MessageErrorProcessingRequest = "Error processing request";
         private const string _MessagePressAnyKeyToExit = "Press any key to exit...";
         private const string _MessageStoppingTheServer = "Stopping the server...";
+        private const string _MessageTheFollowingCommandsAreAvailable = "The following commands are available:";
+        private const string _MessageEnterCode = "Enter code: ";
+        private const string _MessageInvalidInput = "Invalid input";
 
         #endregion
 
+        #region "Members"
+
         private static WebSocketServerSibs server;
-        private static bool isFirstHeartbeat = true;
+        private static readonly ManualResetEvent statusEventReceived = new ManualResetEvent(false);
 
-        /// <summary>
-        /// Listens for user input and sends the input to the WebSocket server.
-        /// </summary>
-        /// <returns>The task object representing the asynchronous operation.</returns>
-        private static async Task ListenForUserInput()
-        {
-            while (true)
-            {
-                // Read the entire line of input asynchronously
-                string input = await Task.Run(() => System.Console.ReadLine());
-
-                // Check if the input is 'q' to stop the server
-                if (input.ToLower() == "q")
-                {
-                    if (server != null && server.IsStarted)
-                    {
-                        System.Console.WriteLine(_MessageStoppingTheServer);
-                        server.Stop(); // Stop the WebSocket server
-                    }
-                    break; // Exit the loop
-                }
-
-                // Process the JSON input
-                try
-                {
-                    await server.SendMessageToClient(input);
-                }
-                catch (Exception ex)
-                {
-                    System.Console.WriteLine($"{_MessageErrorProcessingJSON}: {ex.Message}");
-                }
-            }
-        }
+        #endregion
 
         /// <summary>
         /// The entry point of the application.
         /// </summary>
         /// <param name="args">The command-line arguments.</param>
         /// <returns>The task object representing the asynchronous operation.</returns>
-        static async Task Main()
+        static void Main()
         {
             // Define the WebSocket server prefix
             string prefix = _WebSocketServerPrefix;
 
             // Create an instance of WebSocketServer
             server = new WebSocketServerSibs();
-            //server.ClientConnected += Server_ClientConnected;
+            server.ClientConnected += Server_ClientConnected;
             server.TerminalStatusReqResponseReceived += Server_TerminalStatusReqResponseReceived;
-            server.SetAuthCredentialsReqReceived += Server_SetAuthCredentialsReqReceived;
-            server.EventNotificationReceived += Server_EventNotificationReceived;
-            server.ProcessPaymentReqReceived += Server_ProcessPaymentReqReceived;
-            server.ErrorNotificationReceived += Server_ErrorNotificationReceived;
-            server.HeartbeatNotificationReceived += Server_HeartbeatNotificationReceived;
+            server.PairingReqReceived += Server_PairingReqReceived;
 
             try
             {
@@ -84,10 +55,7 @@ namespace SimpleWebSocketServer.SIBS.Console
                 server.Start(prefix);
 
                 // Listen for user input asynchronously
-                Task userInputTask = ListenForUserInput();
-
-                // Wait for either the user to press 'q' or the WebSocket server to stop
-                await Task.WhenAny(userInputTask);
+                ListenForUserInput();
             }
             catch (Exception ex)
             {
@@ -106,22 +74,140 @@ namespace SimpleWebSocketServer.SIBS.Console
             System.Console.ReadKey();
         }
 
-        private async static void Server_HeartbeatNotificationReceived(object sender, HeartbeatNotification reqResponse)
+        #region "Private Methods"
+
+        /// <summary>
+        /// Listens for user input and sends the input to the WebSocket server.
+        /// </summary>
+        /// <returns>The task object representing the asynchronous operation.</returns>
+        private static void ListenForUserInput()
         {
-            if (isFirstHeartbeat)
+            var serverIsRunning = true;
+
+            while (serverIsRunning)
             {
-                isFirstHeartbeat = false;
-                await server.SendMessageToClient(JsonConvert.SerializeObject(
-                    new TerminalStatusReq()));
+                // Read user input synchronously
+                string input = System.Console.ReadLine();
+
+                // Check if the input is 'q' to stop the server
+                if (input.ToLower() == "q")
+                {
+                    if (server != null && server.IsStarted)
+                    {
+                        System.Console.WriteLine(_MessageStoppingTheServer);
+                        server.Stop(); // Stop the WebSocket server
+                    }
+                    break; // Exit the loop
+                }
+
+                // Parse the input to the enum
+                if (int.TryParse(input, out int commandValue) && Enum.IsDefined(typeof(TerminalCommandOptions), commandValue))
+                {
+                    var command = (TerminalCommandOptions)commandValue;
+
+                    switch (command)
+                    {
+                        case TerminalCommandOptions.SendTerminalStatusRequest:
+                            SendTerminalStatusRequest().Wait();
+                            break;
+                        case TerminalCommandOptions.SendProcessPaymentRequest:
+                            SendProcessPaymentRequest().Wait();
+                            break;
+                        case TerminalCommandOptions.SendPairingRequest:
+                            SendPairingRequest().Wait();
+                            WaitForEvent(statusEventReceived);
+                            break;
+                        case TerminalCommandOptions.ShowListOfCommands:
+                            ShowListOfCommands();
+                            break;
+                        case TerminalCommandOptions.StopTheServer:
+                            if (server != null && server.IsStarted)
+                            {
+                                System.Console.WriteLine(_MessageStoppingTheServer);
+                                server.Stop(); // Stop the WebSocket server
+                            }
+                            serverIsRunning = false;
+                            break;
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine(_MessageInvalidInput);
+                    ShowListOfCommands();
+                }
             }
         }
 
-        //private async static void Server_ClientConnected(object sender, System.EventArgs e)
-        //{
-        //    //await server.SendMessageToClient(JsonConvert.SerializeObject(
-        //    //    new TerminalStatusReq()));
-        //}
+        /// <summary>
+        /// Shows the list of commands.
+        /// </summary>
+        private static void ShowListOfCommands()
+        {
+            System.Console.WriteLine($"{Environment.NewLine}{Environment.NewLine}{_MessageTheFollowingCommandsAreAvailable}");
 
+            foreach (TerminalCommandOptions command in Enum.GetValues(typeof(TerminalCommandOptions)))
+            {
+                System.Console.WriteLine($"   {(int)command} - {Lib.Utilities.UtilitiesSibs.GetEnumDescription(command)}");
+            }
+
+            System.Console.WriteLine($"{Environment.NewLine}");
+        }
+
+        /// <summary>
+        /// Event wait handler
+        /// </summary>
+        /// <param name="eventHandle">The event handle</param>
+        /// <param name="actionName">The action name</param>
+        private static void WaitForEvent(ManualResetEvent eventHandle)
+        {
+            eventHandle.WaitOne();
+            eventHandle.Reset();
+        }
+
+        #endregion
+
+        #region "Server Events"
+
+        /// <summary>
+        /// Event handler for the PairingReqReceived event.
+        /// </summary>
+        /// <param name="sender">The sender object.</param>
+        /// <param name="reqResponse">The PairingReqResponse object.</param>
+        private static async void Server_PairingReqReceived(object sender, PairingReqResponse reqResponse)
+        {
+            try
+            {
+                System.Console.Write(_MessageEnterCode);
+
+                // Read user input synchronously
+                string input = System.Console.ReadLine();
+
+                var pairingReq = new PairingReq() { PairingCode = input, PairingStep = Lib.Enums.Enums.PairingStep.VALIDATE_PAIRING_CODE };
+                await server.SendMessageToClient(JsonConvert.SerializeObject(pairingReq));
+
+                statusEventReceived.Set();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"{_MessageErrorProcessingRequest}: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Event handler for the ClientConnected event.
+        /// </summary>
+        /// <param name="sender">The sender object.</param>
+        /// <param name="e">The EventArgs object.</param>
+        private static void Server_ClientConnected(object sender, EventArgs e)
+        {
+            ShowListOfCommands();
+        }
+
+        /// <summary>
+        /// Event handler for the TerminalStatusReqResponseReceived event.
+        /// </summary>
+        /// <param name="sender">The sender object.</param>
+        /// <param name="reqResponse">The TerminalStatusReqResponse object.</param>
         private async static void Server_TerminalStatusReqResponseReceived(object sender, TerminalStatusReqResponse reqResponse)
         {
             if (reqResponse != null && !reqResponse.HasCredentials)
@@ -132,41 +218,64 @@ namespace SimpleWebSocketServer.SIBS.Console
                         System.Text.Encoding.UTF8.GetString(
                     Properties.Resources.credentials))));
             }
-            else
+        }
+
+        #endregion
+
+        #region "Commands"
+
+        /// <summary>
+        /// Sends a terminal status request.
+        /// </summary>
+        /// <returns>The task object representing the asynchronous operation.</returns>
+        private static async Task SendTerminalStatusRequest()
+        {
+            try
             {
-#if DEBUG
-                // Perform transaction
-                
-                await server.SendMessageToClient(JsonConvert.SerializeObject(
-                    new ProcessPaymentReq
-                    {
-                        AmountData = new AmountData
-                        {
-                            Amount = 0.01,
-                        }
-                    }));
-#endif
+                var terminalStatusRequest = new TerminalStatusReq();
+                await server.SendMessageToClient(JsonConvert.SerializeObject(terminalStatusRequest));
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"{_MessageErrorProcessingRequest}: {ex.Message}");
             }
         }
 
-        private static void Server_SetAuthCredentialsReqReceived(object sender, SetAuthCredentialsReqResponse reqResponse)
+        /// <summary>
+        /// Sends a process payment request.
+        /// </summary>
+        /// <returns>The task object representing the asynchronous operation.</returns>
+        private static async Task SendProcessPaymentRequest()
         {
-            // To be implemented
+            try
+            {
+                var processPaymentRequest = new ProcessPaymentReq { AmountData = new AmountData { Amount = 0.01 } };
+                await server.SendMessageToClient(JsonConvert.SerializeObject(processPaymentRequest));
+                System.Console.WriteLine("Process Payment Request sent.");
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"{_MessageErrorProcessingRequest}: {ex.Message}");
+            }
         }
 
-        private static void Server_ProcessPaymentReqReceived(object sender, ProcessPaymentReqResponse reqResponse)
+        /// <summary>
+        /// Sends a pairing code request.
+        /// </summary>
+        /// <returns>The task object representing the asynchronous operation.</returns>
+        private static async Task SendPairingRequest()
         {
-            // To be implemented
+            try
+            {
+                var pairingReq = new PairingReq();
+                await server.SendMessageToClient(JsonConvert.SerializeObject(pairingReq));
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"{_MessageErrorProcessingRequest}: {ex.Message}");
+            }
         }
 
-        private static void Server_EventNotificationReceived(object sender, EventNotification reqResponse)
-        {
-            // To be implemented
-        }
-
-        private static void Server_ErrorNotificationReceived(object sender, ErrorNotification reqResponse)
-        {
-            // To be implemented
-        }
+        #endregion
     }
 }
---
+++ SimpleWebSocketServer.SIBS/Utilities/UtilitiesSibs.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.ComponentModel;
+using static SimpleWebSocketServer.SIBS.Lib.Enums.Enums;
+
+namespace SimpleWebSocketServer.SIBS.Lib.Utilities
+{
+    public static class UtilitiesSibs
+    {
+        public static string GetEnumDescription(TerminalCommandOptions value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attribute = (DescriptionAttribute)field.GetCustomAttribute(typeof(DescriptionAttribute));
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+    }
+}
