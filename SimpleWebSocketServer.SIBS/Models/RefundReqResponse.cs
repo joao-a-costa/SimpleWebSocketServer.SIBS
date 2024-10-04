e40eec09c4561e7a9ff969aa4f563f3c9993ab5f
João Costa <joaomacostadev@gmail.com>
2024-10-04 18:29:11 +0100

    Add missing fields to RefundReqResponse

--- SimpleWebSocketServer.SIBS/Models/RefundReqResponse.cs
+++ SimpleWebSocketServer.SIBS/Models/RefundReqResponse.cs
@@ -20,8 +20,17 @@ namespace SimpleWebSocketServer.SIBS.Lib.Models
 
     public class RefundData
     {
+        [JsonProperty("authorizationId")]
+        public string AuthorizationId { get; set; }
+
+        [JsonProperty("cardData")]
+        public CardData CardData { get; set; }
+
         [JsonProperty("amount")]
         public double Amount { get; set; }
+
+        [JsonProperty("appId")]
+        public string AppId { get; set; }
 
         [JsonProperty("errorCode")]
         public double ErrorCode { get; set; }
@@ -37,6 +46,21 @@ namespace SimpleWebSocketServer.SIBS.Lib.Models
 
         [JsonProperty("receiptTextsData")]
         public ReceiptTextsData ReceiptTextsData { get; set; }
+
+        [JsonProperty("resultStatus")]
+        public string ResultStatus { get; set; }
+
+        [JsonProperty("sdkDateTime")]
+        public System.DateTime SdkDateTime { get; set; }
+
+        [JsonProperty("sdkId")]
+        public string SdkId { get; set; }
+
+        [JsonProperty("serverDateTime")]
+        public System.DateTime ServerDateTime { get; set; }
+
+        [JsonProperty("serverId")]
+        public string ServerId { get; set; }
 
         [JsonProperty("shouldShowAmountToCardholder")]
         public bool ShouldShowAmountToCardholder { get; set; }
